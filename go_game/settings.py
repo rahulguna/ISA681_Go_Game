5ea42fdbf895d6756e5ce7c048e3043dd3af363e
Rahul Gunasekaran <rahulguna10@gmail.com>
2019-11-22 16:56:46 -0500

    changed to postgres database

--- go_game/settings.py
+++ go_game/settings.py
@@ -65,8 +65,12 @@ WSGI_APPLICATION = 'go_game.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'go_game',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
