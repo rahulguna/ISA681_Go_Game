79860cf2b44da81bf9a3e90caec3062cb727a526
Rahul Gunasekaran <rahulguna10@gmail.com>
2019-12-12 14:49:05 -0500

    fixed vulnerablity

--- go_game/settings.py
+++ go_game/settings.py
@@ -7,7 +7,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '76t_o&oi@e!^@9age&8)1=@0bb71a_(19=)%l4k-k&um96l+w3'
+SECRET_KEY = config('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
